4fead4265490001a8dd3156cdcd88131440e1944
SSL <2300032527@kluniversity.in>
2025-11-28 09:02:45 +0530

    hi

--- FRONTEND/src/components/Signup.jsx
+++ FRONTEND/src/components/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:2025/api/signup", {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/signup`, {
         username,
         name,
         password,
